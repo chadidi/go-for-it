c95dae8245d3326314097ce553447c11350f4c3f
Abdellah Chadidi <chadidi97@gmail.com>
2025-02-10 14:30:05 +0800

    Hello World Example with Binary

---
+++ 001-hello-world/main.go
@@ -0,0 +1,11 @@
+// Package main defines the entry point for the Go program.
+package main
+
+// Importing the fmt package for formatted I/O operations.
+import "fmt"
+
+// main is the entry point of the Go program.
+func main() {
+	// fmt.Println prints the specified message to the console.
+	fmt.Println("Hello World! This is my first GO app.")
+}
